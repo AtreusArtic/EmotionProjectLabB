353ed336bbf1a64329d74d1a00fea2d28c8d6655
AtreusArtic <enrico.artese@gmail.com>
2023-03-12 13:00:48 +0100

    Add GUI folder to implement frontend app

---
+++ EmotionalSongs/src/GUI/javaFX/ProgettoLaboratorioB/GUI/ManagerGUI.java
@@ -0,0 +1,21 @@
+package ProgettoLaboratorioB.GUI;
+
+import ProgettoLaboratorioB.Database.Database;
+
+/**
+ * With this class define the methods to create the GUI of the manager.
+ * IMPO: queste keyword sotto riportate, sono solo d'esempio illustrativo per Marika e Sam
+ * @param List<String> list is the list of the users.
+ * @link https://docs.oracle.com/javase/8/javafx/api/javafx/scene/layout/BorderPane.html
+ * @see Database to see information about the database.
+ */
+public class ManagerGUI {
+    /**
+     * Create the GUI of the manager.
+     */
+    public void ManagerGUI() {
+        // create the GUI of the manager
+    }
+
+
+}
---
+++ EmotionalSongs/src/database/java/ProgettoLaboratorioB/Database/Database.java
@@ -0,0 +1,21 @@
+package ProgettoLaboratorioB.Database;
+/**
+ * With this class define the methods to connect to the database provided by the server.
+ * The database use PostgreSQL library: version 42.2.5.
+ */
+public class Database
+{
+    /**
+     * Connect to the database.
+     */
+    public void DatabaseConnection() {
+        // connect to the database
+    }
+
+    /**
+     * Create the database.
+     */
+    public void CreateDatabase() {
+        // create the database
+    }
+}
