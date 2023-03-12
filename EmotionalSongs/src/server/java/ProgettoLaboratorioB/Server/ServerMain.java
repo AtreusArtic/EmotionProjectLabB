7ccd2e519a307dd85ddac7d65d4a5e974fe7519f
AtreusArtic <enrico.artese@gmail.com>
2023-03-12 11:55:32 +0100

    Update MainServer class

--- EmotionalSongs/src/server/java/ProgettoLaboratorioB/Server/ServerMain.java
+++ EmotionalSongs/src/server/java/ProgettoLaboratorioB/Server/ServerMain.java
@@ -1,5 +1,9 @@
 package ProgettoLaboratorioB.Server;
-
+/**
+ * ServerMain class is the class used to start the server.
+ * The server can use the methods defined in the interface using the remote object of the server.
+ * The remote object is created by the RMI registry.
+ */
 public class ServerMain
 {
     /**
@@ -7,15 +11,19 @@ public class ServerMain
      */
     public static void main(String[] args) {
         /*
-        * 1. CreateDatabase connection;
+        * 1. CreateDatabase connection; */
+        DatabaseConnection();
+        /*
         * 2. Connect the server by initialise
         * */
+        ServerInitialization();
     }
 
     /**
      * Connect to the database.
      */
-    public void DatabaseConnection() {
+    public static void DatabaseConnection()
+    {
         // connect to the database
     }
 
@@ -23,7 +31,8 @@ public class ServerMain
      * Initialize the server, starting the RMI registry and the server
      * in order to be used by the client.
      */
-    public void ServerInitialization() {
+    public static void ServerInitialization()
+    {
         // initialize the server, starting the RMI registry and the server
         // in order to be used by the client
     }
