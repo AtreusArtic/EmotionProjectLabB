ca4bed1d3cc6d53717cd55bc50ec46b4ed41c3be
marikascalise <115414878+marikascalise@users.noreply.github.com>
2023-05-15 12:41:57 +0200

    Add Scene Builder
    Add Scene Builder to creare the GUI of the project

--- EmotionalSongs/src/java/ProgettoLaboratorioB/GUI/ManagerGUI.java
+++ EmotionalSongs/src/java/ProgettoLaboratorioB/GUI/ManagerGUI.java
@@ -1,17 +1,16 @@
 package ProgettoLaboratorioB.GUI;
 
-
 import ProgettoLaboratorioB.Database.Database;
 import javafx.application.Application;
 
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
 
 import javafx.scene.control.Label;
 
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
-
 
 /**
  * With this class define the methods to create the GUI of the manager.
@@ -24,11 +23,14 @@ public class ManagerGUI extends Application{
      * Create the GUI of the manager.
      */
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("FirstMenu.fxml"));
+        Parent root = fxmlLoader.load();
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
         Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setTitle("Emotional Songs");
         stage.setScene(scene);
         stage.show();
     }
