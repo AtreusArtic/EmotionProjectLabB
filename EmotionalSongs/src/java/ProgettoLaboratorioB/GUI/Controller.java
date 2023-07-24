64d011861dc5583f0b832efb9fc291234da81f4b
marikascalise <115414878+marikascalise@users.noreply.github.com>
2023-07-24 17:25:39 +0200

    Update Manager and Add Controller

---
+++ EmotionalSongs/src/java/ProgettoLaboratorioB/GUI/Controller.java
@@ -0,0 +1,18 @@
+package ProgettoLaboratorioB.GUI;
+
+import java.awt.event.ActionEvent;
+
+public class Controller {
+    public void signIn(ActionEvent e){
+        System.out.println("Welcome!");
+    }
+    public void LogIn (ActionEvent e){
+        System.out.println("Welcome Back!");
+    }
+    public void AnonymousMenu (ActionEvent e){
+        System.out.println("Welcome in Anonymous Menu!");
+    }
+    public void Exit(ActionEvent e){
+        System.out.println("GoodBye!");
+    }
+}
--- EmotionalSongs/src/java/ProgettoLaboratorioB/GUI/ManagerGUI.java
+++ EmotionalSongs/src/java/ProgettoLaboratorioB/GUI/ManagerGUI.java
@@ -6,11 +6,10 @@ import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
-
-import javafx.scene.control.Label;
-
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
 
 /**
  * With this class define the methods to create the GUI of the manager.
@@ -23,18 +22,19 @@ public class ManagerGUI extends Application{
      * Create the GUI of the manager.
      */
     @Override
-    public void start(Stage stage) throws Exception{
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("FirstMenu.fxml"));
-        FXMLLoader fxml_Loader = new FXMLLoader(getClass().getResource("/src/java/ProgettoLaboratorioB/GUI/AnonymousMenu.fxml"));
-        Parent root = (Parent) fxmlLoader.load();
-        Parent root1 = (Parent) fxml_Loader.load();
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setTitle("Emotional Songs");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage){
+        try {
+            System.out.println("sta caricando");
+            Parent root = FXMLLoader.load(getClass().getResource("EmotionalSongs/src/java//Users/marikascalise/Documents/GitHub/EmotionProjectLabB/EmotionalSongs/src/java/ProgettoLaboratorioB/GUI/FirstMenu.fxml"));
+            System.out.println("caricato");
+            Scene scene = new Scene(root);
+            //stage.setTitle("Emotional Songs");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static void main(String[] args) {
