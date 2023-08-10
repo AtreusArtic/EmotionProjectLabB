bbcb8ed25d384950df3389c22d6ed35a00091839
AtreusArtic <enrico.artese@gmail.com>
2023-08-10 23:34:03 +0200

    Some JavaFx module loading issues fixed

--- EmotionalSongs/src/java/ProgettoLaboratorioB/GUI/ManagerGUI.java
+++ EmotionalSongs/src/java/ProgettoLaboratorioB/GUI/ManagerGUI.java
@@ -25,13 +25,16 @@ public class ManagerGUI extends Application{
     @Override
     public void start(Stage stage) throws Exception{
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("FirstMenu.fxml"));
-        FXMLLoader fxml_Loader = new FXMLLoader(getClass().getResource("/src/java/ProgettoLaboratorioB/GUI/AnonymousMenu.fxml"));
+        FXMLLoader fxml_Loader = new FXMLLoader(getClass().getResource("AnonymousMenu.fxml"));
         Parent root = (Parent) fxmlLoader.load();
         Parent root1 = (Parent) fxml_Loader.load();
+
+
+
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        Scene scene = new Scene(new StackPane(root), 640, 480);
         stage.setTitle("Emotional Songs");
         stage.setScene(scene);
         stage.show();
