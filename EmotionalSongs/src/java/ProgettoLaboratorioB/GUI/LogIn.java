797e45aea6f92d642d27a13c63c9841d36c14d78
marikascalise <115414878+marikascalise@users.noreply.github.com>
2023-08-24 16:30:15 +0200

    Create LogIn.java

---
+++ EmotionalSongs/src/java/ProgettoLaboratorioB/GUI/LogIn.java
@@ -0,0 +1,9 @@
+package ProgettoLaboratorioB.GUI;
+
+public class LogIn {
+
+    //check if the user exist in the database
+    //if the user exit but he doesn't insert password or username, "please insert your data"
+    //if the user not exist in the database "Your account doesn't exist"
+    //if the user insert the username or password incorrect "wrong username or password"
+}
