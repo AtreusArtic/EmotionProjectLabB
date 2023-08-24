307963c48b1537ca9efe30f76ea510a2709a2638
marikascalise <115414878+marikascalise@users.noreply.github.com>
2023-08-24 16:33:05 +0200

    Update LogIn.java

--- EmotionalSongs/src/java/ProgettoLaboratorioB/GUI/LogIn.java
+++ EmotionalSongs/src/java/ProgettoLaboratorioB/GUI/LogIn.java
@@ -1,9 +1,20 @@
 package ProgettoLaboratorioB.GUI;
 
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
 public class LogIn {
 
     //check if the user exist in the database
     //if the user exit but he doesn't insert password or username, "please insert your data"
     //if the user not exist in the database "Your account doesn't exist"
     //if the user insert the username or password incorrect "wrong username or password"
+
+    public void userLog(ActionEvent actionEvent) throws IOException {
+        checkLogIn();
+    }
+
+    private void checkLogIn() {
+        
+    }
 }
