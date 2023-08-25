19559b76a19474e10890910305cbaa9cccb586be
marikascalise <115414878+marikascalise@users.noreply.github.com>
2023-08-25 16:51:11 +0200

    Update GUI
    Updating Login class as controller of the Login fxml and adding the after login fxml + creating the method to change the scene

--- EmotionalSongs/src/java/ProgettoLaboratorioB/GUI/ManagerGUI.java
+++ EmotionalSongs/src/java/ProgettoLaboratorioB/GUI/ManagerGUI.java
@@ -2,16 +2,15 @@ package ProgettoLaboratorioB.GUI;
 
 import ProgettoLaboratorioB.Database.QueryModule;
 import javafx.application.Application;
-
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
-
 import javafx.scene.control.Label;
-
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * With this class define the methods to create the GUI of the manager.
@@ -20,24 +19,33 @@ import javafx.stage.Stage;
  * @see QueryModule to see information about the database.
  */
 public class ManagerGUI extends Application{
+    private static Stage stg;
     /**
      * Create the GUI of the manager.
      */
     @Override
     public void start(Stage stage) throws Exception{
+        stg = stage;
+        stage.setResizable(false);
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("FirstMenu.fxml"));
-        FXMLLoader fxml_Loader = new FXMLLoader(getClass().getResource("AnonymousMenu.fxml"));
+        //FXMLLoader fxml_Loader = new FXMLLoader(getClass().getResource("AnonymousMenu.fxml"));
         Parent root = (Parent) fxmlLoader.load();
-        Parent root1 = (Parent) fxml_Loader.load();
+        //Parent root1 = (Parent) fxml_Loader.load();
         /*String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
         AnchorPane test = new AnchorPane(l);*/
-        Scene scene = new Scene(new StackPane(root), 1450, 1000);
-        stage.setTitle("Emotional Songs");
+        Scene scene = new Scene(new StackPane(root), 1050, 850);
+        stage.setTitle("Welcome to Emotional Songs!!");
         stage.setScene(scene);
         stage.show();
     }
+
+    public void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stg.getScene().setRoot(pane);
+    }
+
 
     public static void main(String[] args) {
         launch(args);
