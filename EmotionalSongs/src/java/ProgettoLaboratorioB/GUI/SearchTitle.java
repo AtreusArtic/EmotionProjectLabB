7dcb1df8d9ab4df2fe39b0c6e67349c321178617
marikascalise <115414878+marikascalise@users.noreply.github.com>
2023-08-28 22:26:45 +0200

    Create SearchTitle class

---
+++ EmotionalSongs/src/java/ProgettoLaboratorioB/GUI/SearchTitle.java
@@ -0,0 +1,13 @@
+package ProgettoLaboratorioB.GUI;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+
+public class SearchTitle {
+    @FXML
+    public TextField search_Title;
+
+}
