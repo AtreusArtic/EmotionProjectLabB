8d1cfbf1032b998b6b31cfab291c28c2964c2109
marikascalise <115414878+marikascalise@users.noreply.github.com>
2023-08-31 15:06:08 +0200

    Set Emotion GUI

--- EmotionalSongs/src/java/ProgettoLaboratorioB/GUI/ControllerClass/PlaylistController.java
+++ EmotionalSongs/src/java/ProgettoLaboratorioB/GUI/ControllerClass/PlaylistController.java
@@ -3,6 +3,7 @@ package ProgettoLaboratorioB.GUI.ControllerClass;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 import java.awt.*;
 
@@ -12,23 +13,19 @@ public class PlaylistController {
     @FXML
     public Button delete_Playlist_btn;
     @FXML
-    private Button show_Playlist_btn;
+    public Label lbl_empty;
     @FXML
-    public Button update_Playlist_btn;
-
-    public void show_User_Playlist(javafx.event.ActionEvent actionEvent) {
-    }
-
-
-    public void update_User_playlist(ActionEvent actionEvent) {
-    }
+    public Label lbl_notesist;
 
 
     public void delete_Playlist(ActionEvent actionEvent) {
+        //The inserted playlist name does not exist!! - Label lbl_notesist message
     }
 
 
     public void add_Playlist(ActionEvent actionEvent) {
+
+        //Enter the name of the playlist - Label lbl_empty message
 
         
     }
