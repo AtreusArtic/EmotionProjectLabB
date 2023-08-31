c5c706d5c06aaf57d6390d355cf9ccde73b7489e
marikascalise <115414878+marikascalise@users.noreply.github.com>
2023-08-31 14:14:54 +0200

    Lay the foundation for Playlist features

---
+++ EmotionalSongs/src/java/ProgettoLaboratorioB/GUI/ControllerClass/PlaylistController.java
@@ -0,0 +1,35 @@
+package ProgettoLaboratorioB.GUI.ControllerClass;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.awt.*;
+
+public class PlaylistController {
+    @FXML
+    public Button create_Playlist_btn;
+    @FXML
+    public Button delete_Playlist_btn;
+    @FXML
+    private Button show_Playlist_btn;
+    @FXML
+    public Button update_Playlist_btn;
+
+    public void show_User_Playlist(javafx.event.ActionEvent actionEvent) {
+    }
+
+
+    public void update_User_playlist(ActionEvent actionEvent) {
+    }
+
+
+    public void delete_Playlist(ActionEvent actionEvent) {
+    }
+
+
+    public void add_Playlist(ActionEvent actionEvent) {
+
+        
+    }
+}
