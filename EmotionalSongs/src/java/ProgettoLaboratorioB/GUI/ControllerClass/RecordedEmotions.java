5b98974aa0da96b7bdf302f2abb4dc7130e430cb
marikascalise <115414878+marikascalise@users.noreply.github.com>
2023-09-04 11:19:57 +0200

    Recorded Emotions

---
+++ EmotionalSongs/src/java/ProgettoLaboratorioB/GUI/ControllerClass/RecordedEmotions.java
@@ -0,0 +1,100 @@
+package ProgettoLaboratorioB.GUI.ControllerClass;
+
+import ProgettoLaboratorioB.GUI.ManagerGUI;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RecordedEmotions extends ManagerGUI implements Initializable {
+    @FXML
+    public Label sole_Score_lbl;
+    @FXML
+    public Label amaz_Score_lbl;
+    @FXML
+    public Label tend_Score_lbl;
+    @FXML
+    public Label nost_Score_lbl;
+    @FXML
+    public Label calm_Score_lbl;
+    @FXML
+    public Label pow_Score_lbl;
+    @FXML
+    public Label joy_Score_lbl;
+    @FXML
+    public Label tens_Score_lbl;
+    @FXML
+    public Label sad_Score_lbl;
+    @FXML
+    public Label amaz_Note_lbl;
+    @FXML
+    public Label sole_Note_lbl;
+    @FXML
+    public Label tend_Note_lbl;
+    @FXML
+    public Label nost_Note_lbl;
+    @FXML
+    public Label calm_Note_lbl;
+    @FXML
+    public Label joy_Note_lbl;
+    @FXML
+    public Label pow_Note_lbl;
+    @FXML
+    public Label ten_Note_lbl;
+    @FXML
+    public Label sad_Note_lbl;
+    @FXML
+    public Button back_Menu_btn;
+    @FXML
+    public Button back_btn;
+    @FXML
+    public Button forward_btn;
+    @FXML
+    public Label sole_Tot_lbl;
+    @FXML
+    public Label tend_Tot_lbl;
+    @FXML
+    public Label nost_Tot_lbl;
+    @FXML
+    public Label calm_Tot_lbl;
+    @FXML
+    public Label pow_Tot_lbl;
+    @FXML
+    public Label joy_Tot_lbl;
+    @FXML
+    public Label ten_Tot_lbl;
+    @FXML
+    public Label sad_Tot_lbl;
+    @FXML
+    public Label amaz_Users_lbl;
+    @FXML
+    public Label sole_Users_lbl;
+    @FXML
+    public Label tend_Users_lbl;
+    @FXML
+    public Label nost_Users_lbl;
+    @FXML
+    public Label calm_Users_lbl;
+    @FXML
+    public Label pow_Users_lbl;
+    @FXML
+    public Label joy_Users_lbl;
+    @FXML
+    public Label ten_Users_lbl;
+    @FXML
+    public Label sad_Users_lbl;
+    @FXML
+    public Label name_Song_lbl;
+    @FXML
+    public Button get_Song_btn;
+    @FXML
+    public Label username_lbl;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
